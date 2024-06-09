d99c9efc4c6e00ff54ae13eab483a7464883d41f
Vinicius M. Almeida <vinicius.sec21@gmail.com>
2024-06-08 21:27:14 -0300

    fix config file, add gitignore

--- db/connection.go
+++ db/connection.go
@@ -11,10 +11,10 @@ import (
 func OpenConnection() (*sql.DB, error) {
 	conf := configs.GetDB()
 
-	sc := fmt.Sprintf("host=%s port=%s user=%s pass=%s dbname=%s sslmode=disable",
+	sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		conf.Host, conf.Port, conf.User, conf.Pass, conf.Database)
 
-	conn, err := sql.Open("Postgres", sc)
+	conn, err := sql.Open("postgres", sc)
 
 	if err != nil {
 		panic(err)
